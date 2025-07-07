fffe10e81b447fb633355c1a74c7ac5a7cfdd78d
sumit patidar <120245858+sumitpatidar11@users.noreply.github.com>
2025-07-07 21:22:51 +0530

    parse.py

--- parse.py
+++ parse.py
@@ -1,30 +1,3 @@
-# from langchain_ollama import OllamaLLM
-# from langchain_core.prompts import ChatPromptTemplate
-
-# template = (
-#     "give me all the reviews and rating of this product which is given by customers on the product  "
-# )
-
-# model = OllamaLLM(model="moondream")
-
-
-# def parse_with_ollama(dom_chunks, parse_description):
-#     prompt = ChatPromptTemplate.from_template(template)
-#     chain = prompt | model
-
-#     parsed_results = []
-
-#     for i, chunk in enumerate(dom_chunks, start=1):
-#         response = chain.invoke(
-#             {"dom_content": chunk, "parse_description": parse_description}
-#         )
-#         print(f"Parsed batch: {i} of {len(dom_chunks)}")
-#         parsed_results.append(response)
-
-#     return "\n".join(parsed_results)
-
-
-
 
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
